a66be0978e9a5824cc71006f18ee0cf5fb61cf6b
FlorentinLeku <fl47298@ubt-uni.net>
2022-08-02 17:20:36 +0200

    3RD Commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer()
+{
+    return(
+        <React.Fragment>
+            <h4>This is Footer</h4>
+        </React.Fragment>
+    );
+}
+
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header()
+{
+    return(
+        <React.Fragment>
+            <h4>This is Header</h4>
+        </React.Fragment>
+    );
+}
+
